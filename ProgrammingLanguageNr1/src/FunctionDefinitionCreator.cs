fff0c0597d984db2273d9d4613e6b74f018c61b3
Erik <erik.svedang@gmail.com>
2013-05-29 15:33:09 +0200

    Auto convertion of arguments to functions (probably a bad idea but no more crashing)

--- ProgrammingLanguageNr1/src/FunctionDefinitionCreator.cs
+++ ProgrammingLanguageNr1/src/FunctionDefinitionCreator.cs
@@ -33,96 +33,129 @@ namespace ProgrammingLanguageNr1
 		{
 			//Log("Creating function definition targeted on " + pProgramTarget.ToString () + " on class of type " + pClassType.ToString ());
 
-            List<FunctionDefinition> functionDefinitions = new List<FunctionDefinition>();
-            Dictionary<string, FunctionDocumentation> functionDocumentations = new Dictionary<string, FunctionDocumentation>();
             MethodInfo[] methodInfos = pClassType.GetMethods();
 
-            foreach (MethodInfo methodInfo in methodInfos)
-            {
-				//Log("Found method " + methodInfo.Name);
-
-                if (methodInfo.Name.StartsWith("API_"))
-                {
-                    SprakAPI[] helpAttributes = (SprakAPI[])methodInfo.GetCustomAttributes(typeof(SprakAPI), true);
-                    if (helpAttributes.Length > 0)
-                    {
-                        //Console.WriteLine("found " + String.Join( ",", help[0].Values));
-                        List<string> parameterHelp = new List<string>();
-                        for (int i = 1; i < helpAttributes[0].Values.Length; i++) {
-                            parameterHelp.Add(helpAttributes[0].Values[i]);
-						}
-                        string shortname = methodInfo.Name.Substring(4);
-                        FunctionDocumentation fd = new FunctionDocumentation(helpAttributes[0].Values[0], parameterHelp.ToArray());
-                        functionDocumentations.Add(shortname, fd);
-                    }
-                }
-            }
-
-            foreach (MethodInfo methodInfo in methodInfos)
-            {
-                if (methodInfo.Name.StartsWith("API_"))
-                {
-                    //Console.WriteLine("parsing " + mi.Name + " return Type " + mi.ReturnType.Name);
-                    string shortname = methodInfo.Name.Substring(4);
-
-					if (methodInfo.ReturnType.IsArray) {
-						throw new Exception ("FunctionDefinitionCreator can't handle array return value");
-					}
-
-                    List<ReturnValueType> parameterTypes = new List<ReturnValueType>();
-                    List<string> parameterNames = new List<string>();
-                    List<string> parameterTypeNames = new List<string>();
-
-                    foreach (ParameterInfo parameterInfo in methodInfo.GetParameters())
-                    {
-						if (parameterInfo.ParameterType.IsArray) {
-							throw new Exception ("FunctionDefinitionCreator can't handle array parameters");
-						}
-                        parameterNames.Add(parameterInfo.Name);
-                        parameterTypes.Add(ReturnValue.SystemTypeToReturnValueType(parameterInfo.ParameterType));
-                        parameterTypeNames.Add(ReturnValue.SystemTypeToReturnValueType(parameterInfo.ParameterType).ToString().ToLower());
-                    }
-
-                    MethodInfo lamdaMethodInfo = methodInfo;
-                    ExternalFunctionCreator.OnFunctionCall function = (retvals) =>
-                    {
-                        int i = 0;
-                        ParameterInfo[] realParamInfo = lamdaMethodInfo.GetParameters();
-                        List<object> parameters = new List<object>();
-
-                        foreach (ReturnValue r in retvals)
-                        {
-                            if (realParamInfo[i++].ParameterType == typeof(int)) {
-                                parameters.Add(Convert.ToInt32(r.Unpack()));
-							}
-                            else {
-                                parameters.Add(r.Unpack());
-							}
-                        }
-
-                        //Console.WriteLine("supplied parameter count" + parameters.Count + " neededParamter count " + lamdaMethodInfo.GetParameters().Length);
-                        object result = lamdaMethodInfo.Invoke(pProgramTarget, parameters.ToArray());
-
-                        if (lamdaMethodInfo.ReturnType == typeof(void)) {
-                            return new ReturnValue(ReturnValueType.VOID);
-						}
-                        else {
-                            return new ReturnValue(ReturnValue.SystemTypeToReturnValueType(lamdaMethodInfo.ReturnType), result);
-                        }
-                    };
-
-                    ReturnValueType returnValueType = ReturnValue.SystemTypeToReturnValueType(methodInfo.ReturnType);
-                    
-					FunctionDocumentation doc;
-					if (functionDocumentations.TryGetValue (shortname, out doc)) {
-						functionDefinitions.Add (new FunctionDefinition(returnValueType.ToString(), shortname, parameterTypeNames.ToArray(), parameterNames.ToArray(), function, doc));
-					} else {
-						functionDefinitions.Add (new FunctionDefinition(returnValueType.ToString(), shortname, parameterTypeNames.ToArray(), parameterNames.ToArray(), function, FunctionDocumentation.Default()));
-					}
-                }
-            }
+			var documentation = CreateDocumentation (methodInfos);
+            var functionDefinitions = CreateFunctionDefinitions (pProgramTarget, documentation, methodInfos);
 
             return functionDefinitions.ToArray();
         }
+
+		static Dictionary<string, FunctionDocumentation> CreateDocumentation (MethodInfo[] methodInfos)
+		{
+			Dictionary<string, FunctionDocumentation> functionDocumentations = new Dictionary<string, FunctionDocumentation>();
+
+			foreach (MethodInfo methodInfo in methodInfos) {
+				//Log("Found method " + methodInfo.Name);
+				if (methodInfo.Name.StartsWith ("API_")) {
+					SprakAPI[] helpAttributes = (SprakAPI[])methodInfo.GetCustomAttributes (typeof(SprakAPI), true);
+					if (helpAttributes.Length > 0) {
+						//Console.WriteLine("found " + String.Join( ",", help[0].Values));
+						List<string> parameterHelp = new List<string> ();
+						for (int i = 1; i < helpAttributes [0].Values.Length; i++) {
+							parameterHelp.Add (helpAttributes [0].Values [i]);
+						}
+						string shortname = methodInfo.Name.Substring (4);
+						FunctionDocumentation fd = new FunctionDocumentation (helpAttributes [0].Values [0], parameterHelp.ToArray ());
+						functionDocumentations.Add (shortname, fd);
+					}
+				}
+			}
+
+			return functionDocumentations;
+		}
+
+		static List<FunctionDefinition> CreateFunctionDefinitions (object pProgramTarget, Dictionary<string, FunctionDocumentation> functionDocumentations, MethodInfo[] methodInfos)
+		{
+			List<FunctionDefinition> functionDefinitions = new List<FunctionDefinition>();
+
+			foreach (MethodInfo methodInfo in methodInfos) 
+			{
+				if (!methodInfo.Name.StartsWith ("API_")) {
+					continue;
+				}
+
+				if (methodInfo.ReturnType.IsArray) {
+					throw new Exception ("FunctionDefinitionCreator can't handle array return value");
+				}
+
+				//Console.WriteLine("parsing " + mi.Name + " return Type " + mi.ReturnType.Name);
+				string shortname = methodInfo.Name.Substring (4);
+	
+				List<ReturnValueType> parameterTypes = new List<ReturnValueType> ();
+				List<string> parameterNames = new List<string> ();
+				List<string> parameterTypeNames = new List<string> ();
+
+				foreach (ParameterInfo parameterInfo in methodInfo.GetParameters ()) {
+					if (parameterInfo.ParameterType.IsArray) {
+						throw new Exception ("FunctionDefinitionCreator can't handle array parameters");
+					}
+					parameterNames.Add (parameterInfo.Name);
+					parameterTypes.Add (ReturnValue.SystemTypeToReturnValueType (parameterInfo.ParameterType));
+					parameterTypeNames.Add (ReturnValue.SystemTypeToReturnValueType (parameterInfo.ParameterType).ToString ().ToLower ());
+				}
+
+				MethodInfo lamdaMethodInfo = methodInfo; // "hard copy" because of c# lambda rules
+
+				ExternalFunctionCreator.OnFunctionCall function = (sprakArguments =>  {
+
+					ParameterInfo[] realParamInfo = lamdaMethodInfo.GetParameters ();
+					List<object> parameters = new List<object> ();
+
+					int i = 0;
+					foreach (ReturnValue sprakArg in sprakArguments) {
+						Console.WriteLine(string.Format("Argument {0} in function {1} is of type {2}", i, shortname, realParamInfo[i].ParameterType));
+
+						var realParamType = realParamInfo [i].ParameterType;
+
+						if (realParamType == typeof(int)) {
+							parameters.Add (Convert.ToInt32 (sprakArg.NumberValue));
+						}
+						else if (realParamType == typeof(float)) {
+							parameters.Add (sprakArg.NumberValue);
+						}
+						else if (realParamType == typeof(bool)) {
+							parameters.Add (sprakArg.BoolValue);
+						}
+						else if (realParamType == typeof(string)) {
+							parameters.Add (sprakArg.StringValue);
+						}
+						else {
+							throw new Error("Can't deal with arg " + i.ToString() + " in function " + shortname);
+						}
+						i++;
+					}
+
+					//Console.WriteLine("supplied parameter count" + parameters.Count + " neededParamter count " + lamdaMethodInfo.GetParameters().Length);
+					object result = lamdaMethodInfo.Invoke (pProgramTarget, parameters.ToArray ());
+					if (lamdaMethodInfo.ReturnType == typeof(void)) {
+						return new ReturnValue (ReturnValueType.VOID);
+					}
+					else {
+						return new ReturnValue (ReturnValue.SystemTypeToReturnValueType (lamdaMethodInfo.ReturnType), result);
+					}
+				});
+
+				ReturnValueType returnValueType = ReturnValue.SystemTypeToReturnValueType (methodInfo.ReturnType);
+
+				FunctionDocumentation doc;
+
+				if (functionDocumentations.ContainsKey (shortname)) {
+					doc = functionDocumentations [shortname];
+				} else {
+					doc = FunctionDocumentation.Default ();
+				}
+
+				functionDefinitions.Add (new FunctionDefinition (
+					returnValueType.ToString (), 
+					shortname, 
+					parameterTypeNames.ToArray (), 
+					parameterNames.ToArray (), 
+					function, // The lambda
+					doc));
+			}
+
+			return functionDefinitions;
+		}
     }
 }
